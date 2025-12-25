dd8c5c308d422573b3906aa3f585d2c08cfe14cf
Vivek kumar <vivek.kumar1@rungta.org>
2025-12-25 12:24:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        document.getElementById("display").value =
+            eval(document.getElementById("display").value);
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
